0d5e45fefc29105c52b5cc673c8fcf38d2eac6a3
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2024-04-05 15:45:36 +0900

    flutter 버튼넣기까지

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,20 +8,106 @@ void main() {
 
 //위젯을 만들기 위해 flutter SDK에 있는 3개의 core Widget 중 하나를 상속받아야한다.
 class App extends StatelessWidget {
+  const App({super.key});
+
+  // const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('hello world'),
-        ),
-        appBar: AppBar(
-          title: Text('hello flutter~'),
-          centerTitle: false,
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 120,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Column(),
+                Column(
+                  children: [
+                    Text(
+                      'Hey,Eunsun',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.w800),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total Balance',
+              style: TextStyle(
+                fontSize: 22,
+                color: Color.fromRGBO(250, 250, 250, 0.8),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$5 194 482',
+              style: TextStyle(
+                fontSize: 44,
+                fontWeight: FontWeight.w800,
+                color: Color.fromRGBO(250, 250, 250, 1),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45)),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 50,
+                    ),
+                    child: Text(
+                      'Transfer',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45)),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 50,
+                    ),
+                    child: Text(
+                      'Request',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
-    );
+    ));
 //metarial이나 cupertino 위젯을 리턴해야한다. 어떤 family를 사용할것이냐. 난 둘다싫어? 그래도 선택해야함. root이므로
 //flutter는 google꺼라서 meterial 앱이 더 보기 좋다. ios도 좋지만..  material선택하기
   }
