1acc280376ff73e8f0197ef48741c7f8a5bc483a
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2024-04-17 22:35:34 +0900

    4.0

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -7,17 +7,46 @@ void main() {
 }
 
 //위젯을 만들기 위해 flutter SDK에 있는 3개의 core Widget 중 하나를 상속받아야한다.
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   // const App({super.key});
+  int counter = 0; //이건 final이 아니다. 변경이 가능한 값이니까
+  void onClicked() {
+    counter += 1;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
       backgroundColor: Color(0xFFF4EDDB),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Click Count',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              '$counter',
+              style: TextStyle(fontSize: 30),
+            ),
+            IconButton(
+                iconSize: 30,
+                onPressed: onClicked,
+                icon: Icon(
+                  Icons.add_box_rounded,
+                ))
+          ],
+        ),
+      ),
     ));
   }
 }
