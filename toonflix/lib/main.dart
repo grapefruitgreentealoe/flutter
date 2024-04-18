1120e988f511e3faa39049b8d12e31dd61aab6e3
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2024-04-18 15:09:17 +0900

    4.1~4.5 stateful widgets

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -16,37 +16,73 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   // const App({super.key});
-  int counter = 0; //이건 final이 아니다. 변경이 가능한 값이니까
+  // int counter = 0; //이건 final이 아니다. 변경이 가능한 값이니까
+  // counter는 dart 클래스의 프로퍼티이지 , 상태관리를 하는 flutter의 기능이 아니다.
+  List<int> numbers = [];
+  bool showTitle = true;
+
   void onClicked() {
-    counter += 1;
+    setState(() {
+      // counter += 1;
+      numbers.add(numbers.length);
+    });
+  }
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Color(0xFFF4EDDB),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Click Count',
-              style: TextStyle(fontSize: 30),
-            ),
-            Text(
-              '$counter',
-              style: TextStyle(fontSize: 30),
-            ),
-            IconButton(
-                iconSize: 30,
-                onPressed: onClicked,
-                icon: Icon(
-                  Icons.add_box_rounded,
-                ))
-          ],
+        theme: ThemeData(
+          textTheme: TextTheme(titleLarge: TextStyle(color: Colors.red)),
         ),
-      ),
-    ));
+        home: Scaffold(
+          backgroundColor: Color(0xFFF4EDDB),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                showTitle ? MyLargeTitle() : Text('nothing'),
+                IconButton(
+                    onPressed: toggleTitle, icon: Icon(Icons.remove_red_eye))
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState(); //부모의 initState
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+          fontSize: 30, color: Theme.of(context).textTheme.titleLarge?.color),
+    );
   }
 }
